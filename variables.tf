184221d305c752a429bc38715a869412795c1b0d
aswinsivaram <aswinsivaram445@gmail.com>
2025-09-16 18:29:09 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "region" {
-     default     = "us-east-1"
+     default = "us-east-1"
 }
 variable "vpc_cidr" {
-   default     = "10.0.0.0/16"
+   default = "10.0.0.0/16"
 }
-
 variable "vpc_name" {
-  default     = "main-vpc"
+  default = "main-vpc"
 }
 
