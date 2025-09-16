91da32698efe2ccbd5c0d6f89cc0ae5da5ac917b
aswinsivaram <aswinsivaram445@gmail.com>
2025-09-16 20:12:14 +0000

    Commit with new files added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+  default     = "main-vpc"
+}
+
+variable "region" {
+    default=us-east-1
+}
